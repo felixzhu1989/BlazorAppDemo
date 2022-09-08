329cc62450a5910f98658b59e9b783aaa614e726
felixzhu1989 <zhu-hongfeng@hotmail.com>
2022-09-08 21:39:32 +0800

    js

--- BlazingPizza/Services/OrderState.cs
+++ BlazingPizza/Services/OrderState.cs
@@ -15,9 +15,10 @@ public class OrderState
         {
             Special = special,
             SpecialId = special.Id,
-            Size = Pizza.DefaultSize,
+            //Size = Pizza.DefaultSize,
             Toppings = new List<PizzaTopping>(),
-        };
+        }; 
+        ConfiguringPizza.Size=ConfiguringPizza.DefaultSize;       
         ShowingConfigureDialog = true;
     }
 
