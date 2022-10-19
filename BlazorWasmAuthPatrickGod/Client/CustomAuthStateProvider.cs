192fc50e4e46965997a5577c35fc07cc18c72f17
朱宏峰 <zhu-hongfeng@hotmail.com>
2022-10-19 15:28:40 +0800

    ccc

---
+++ BlazorWasmAuthPatrickGod/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Text.Json;
+using Blazored.LocalStorage;
+using System.Net.Http.Headers;
+
+namespace BlazorWasmAuthPatrickGod.Client
+{
+    public class CustomAuthStateProvider:AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage,HttpClient http)
+        {
+            _localStorage = localStorage;
+            _http = http;
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //从其他地方拷贝一个jwtToken
+            //string token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZmVsaXgiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE2NjIzNjg1MTN9.ac22CCtqdkaIhV0wyXRoLSZy1VT6Dl9a-CSQdSYUEVU";
+
+            string token =await _localStorage.GetItemAsStringAsync("token");
+            var identity = new ClaimsIdentity(); 
+            _http.DefaultRequestHeaders.Authorization = null;
+            if (!string.IsNullOrEmpty(token))
+            {
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+                //给http的请求头中添加token
+                _http.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", token.Replace("\"", ""));
+            }
+            var user = new ClaimsPrincipal(identity);
+            var state = new AuthenticationState(user);
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+            return state;
+        }
+
+        #region 解析Token内容的代码
+        public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var payload = jwt.Split('.')[1];
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+            return keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString()));
+        }
+        private static byte[] ParseBase64WithoutPadding(string base64)
+        {
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return Convert.FromBase64String(base64);
+        } 
+        #endregion
+    }
+}
---
+++ BlazorWasmAuthPatrickGod/Server/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using BlazorWasmAuthPatrickGod.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorWasmAuthPatrickGod.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<string>> Login(UserLoginDto request)
+        {
+            //硬编码，这里只想返回token，可以参考其他项目实现如何生成token
+            string token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjEiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZmVsaXgiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3ZlcnNpb24iOiIyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE2NjIzNjg1MTN9.ac22CCtqdkaIhV0wyXRoLSZy1VT6Dl9a-CSQdSYUEVU";
+            return token;
+        }
+    }
+}
